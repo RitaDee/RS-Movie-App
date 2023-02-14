5818e596f2eb70f180adec4121489f1442f6b7bc
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-02-14 14:26:52 +0100

    Modify stylesheet

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,3 +1,5 @@
+import movieCount from './total.js';
+
 const Movies = document.querySelector('.main-show');
 
 const showMovie = (allShows) => {
@@ -6,18 +8,22 @@ const showMovie = (allShows) => {
     const template = `
   <div class="article">
   <div class="img">
-    <img src=${show.image.medium}
+    <img src=${show.image.medium}>
   </div>
   <div class="name"> <p>${show.name}</p></div>
   <div id="like-r">
   <p><i class="fa-regular fa-heart"></i></p>
   <p class="likes"> <span>0</span> likes</p>
   </div>
-  <div>
+  <div id="btn-ctn">
   <button id="comment">comment</button>
   </div>
   </div>`;
     Movies.innerHTML += template;
+
+    const num = document.querySelector('#movie-counter');
+
+    movieCount(Movies, num);
   });
 };
 
--- src/modules/total.js
+++ src/modules/total.js
@@ -1,3 +1,7 @@
-const totalMovie = (list) => list.length;
+const movieCount = (Movies, num) => {
+  const items = Array.from(Movies.children);
+  num.innerText = items.length;
+  return items;
+};
 
-export default totalMovie;
+export default movieCount;
