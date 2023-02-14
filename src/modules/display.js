70e48ace3911fa90f43a7d79aa47aa93a34d37f0
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-02-14 09:49:55 +0100

    Modify display.js file

--- src/modules/display.js
+++ src/modules/display.js
@@ -3,10 +3,24 @@ import totalMovie from './total.js';
 
 const counter = document.querySelector('#movie-counter');
 
-const showMovies = (Movies) => {
-  Movies.forEach((show) => {
+const showMovie = (arr) => {
+  const Movies = document.querySelector('.main-show');
+  const template = `
+  <div class="article">
+  <div class="img">
+    <img src=${arr.show.image.medium}>
+  </div>
+  <div class="name"> <p> ${arr.show.name} </p> </div>
+  <div class="like-r>
+  <p><i class="fa-regular fa-heart"></i></p>
+  <p class="likes"> <span>0</span> likes</p>
+  </div>
+  <div>
+  <button id="comment">comment</button>
+  </div>
 
-  });
+
+  </div>`;
 };
 
 const displayMovie = async () => {
