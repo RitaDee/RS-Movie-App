2b0deb86bf6652f096dcf62d1c241b096da5c8d9
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-02-14 13:14:49 +0100

    Modify js file

--- src/modules/display.js
+++ src/modules/display.js
@@ -8,8 +8,8 @@ const showMovie = (allShows) => {
   <div class="img">
     <img src=${show.image.medium}
   </div>
-  <div class="name"> <p>${show.name}</p> </div>
-  <div class="like-r>
+  <div class="name"> <p>${show.name}</p></div>
+  <div id="like-r">
   <p><i class="fa-regular fa-heart"></i></p>
   <p class="likes"> <span>0</span> likes</p>
   </div>
